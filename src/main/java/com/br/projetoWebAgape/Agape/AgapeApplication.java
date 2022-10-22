e3bf6e0532cc85cb80abbffd1a02bb334bdaba75
joemyson <joemyson1402@gmail.com>
2022-10-22 15:31:13 -0300

    projeto iniciado

---
+++ src/main/java/com/br/projetoWebAgape/Agape/AgapeApplication.java
@@ -0,0 +1,13 @@
+package com.br.projetoWebAgape.Agape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgapeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/projetoWebAgape/Agape/AgapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.projetoWebAgape.Agape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
